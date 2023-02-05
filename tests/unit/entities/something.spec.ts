8e23492723f51d2b52a2041327dd7fde27703d10
JohnLDev <john.lenon.dev@gmail.com>
2023-02-05 00:08:28 -0300

    feat: create user entity

--- tests/unit/entities/something.spec.ts
+++
@@ -1,9 +0,0 @@
-import { sum } from '../../../src/shared/utils/sum'
-
-describe('something', () => {
-  it('should do something', () => {
-    expect(true).toEqual(true)
-
-    expect(sum(1, 2)).toEqual(3)
-  })
-})

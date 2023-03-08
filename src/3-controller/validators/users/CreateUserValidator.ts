e1b4ff7f415610727eac73a9664bd72d1911c5e0
JohnLDev <john.lenon.dev@gmail.com>
2023-03-08 18:55:09 -0300

    feat: database comunication

--- src/3-controller/validators/users/CreateUserValidator.ts
+++ src/3-controller/validators/users/CreateUserValidator.ts
@@ -1,10 +1,11 @@
 import { type InputCreateUserUseCaseDTO } from '@business/dtos/useCases/user/createUserDTO'
 import { Input } from '@framework/utility/input'
-import { IsEmail, IsNotEmpty, IsNumberString, IsString, MinLength } from 'class-validator'
+import { IsEmail, IsNotEmpty, IsNumberString, IsString, MaxLength, MinLength } from 'class-validator'
 
 export class InputCreateUser extends Input implements InputCreateUserUseCaseDTO {
   @IsString()
   @IsNotEmpty()
+  @MinLength(4)
   name: string
 
   @IsString()
@@ -18,6 +19,8 @@ export class InputCreateUser extends Input implements InputCreateUserUseCaseDTO 
 
   @IsNumberString()
   @IsNotEmpty()
+  @MaxLength(11)
+  @MinLength(11)
   documentNumber: string
 
   @IsString()
--- src/4-framework/handlers/users/createUserHandler.ts
+++ src/4-framework/handlers/users/createUserHandler.ts
@@ -1,16 +1,18 @@
+import { CreateUserUseCase } from '@business/useCases/user/createUser'
 import { InputCreateUser } from '@controller/validators/users/CreateUserValidator'
+import { UserRepository } from '@framework/repositories/userRepository'
+import { HashService } from '@framework/services/hashService'
+import { LoggerService } from '@framework/services/loggerService'
 import { httpHandler } from '@framework/utility/httpHandler'
 import { type Request, type Response } from 'express'
 
 async function handler(req: Request, res: Response): Promise<Response> {
-  return await httpHandler(req, res, async () => {
-    const { username, password } = req.body
-    console.log(req.body)
-    // const user = createUser(username, password)
+  return await httpHandler(req, res, async (): Promise<any> => {
     const input = new InputCreateUser(req.body)
-    console.log(input)
+    const useCase = new CreateUserUseCase(new UserRepository(), new HashService(), new LoggerService())
+    const result = await useCase.exec(input)
 
-    return { user: { username, password }, status: 201 }
+    return { result: result.value, status: result.isFail() ? result.value.status : 201 }
   })
 }
 

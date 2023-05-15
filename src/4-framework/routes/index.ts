e85f7891a0c84a19ade9fc158feefc3da0852419
JohnLDev <john.lenon.dev@gmail.com>
2023-05-15 20:19:21 -0300

    feat: add cors middleware

--- src/4-framework/routes/index.ts
+++ src/4-framework/routes/index.ts
@@ -2,9 +2,10 @@ import { msName } from '@framework/config/domain'
 import { loggerMiddleware } from '@framework/middlewares/logger'
 import { userRoutes } from '@framework/routes/users'
 import express from 'express'
-
+import cors from 'cors'
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use(loggerMiddleware)
 
 app.use(`/${msName}/v1`, userRoutes)
